12908a68c2c222cc207d8fdb78137ef9dd09e4af
ゆうと <106125064+yuto0914@users.noreply.github.com>
2023-06-21 09:48:19 +0900

    Add files via upload

---
+++ work folder /table sample code/main.js
@@ -0,0 +1,24 @@
+console.log("main.js!!");
+// ---------------------------------------------------------------------------------------------------------------
+// マウスポインターの設定コード
+// ---------------------------------------------------------------------------------------------------------------
+const stalker = document.getElementById('mouse-stalker');
+let hovFlag = false;
+
+document.addEventListener('mousemove', function (e) {
+    stalker.style.transform = 'translate(' + e.clientX + 'px, ' + e.clientY + 'px)';
+});
+
+const linkElem = document.querySelectorAll('a:not(.no_stick_)');
+for (let i = 0; i < linkElem.length; i++) {
+    linkElem[i].addEventListener('mouseover', function (e) {
+        hovFlag = true;
+        stalker.classList.add('is_active');
+
+    });
+    linkElem[i].addEventListener('mouseout', function (e) {
+        hovFlag = false;
+        stalker.classList.remove('is_active');
+    });
+}
+// ----------------------------------------------------------------------------------------------------------------
